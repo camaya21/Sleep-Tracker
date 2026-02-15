c8423999a8dbc5501a5724260e7607b1b02a305c
Cindy Amaya <117056137+camaya21@users.noreply.github.com>
2026-02-14 20:23:27 -0500

    Worked on sign up and login

--- sleep-tracker-app/src/App.jsx
+++ sleep-tracker-app/src/App.jsx
@@ -1,40 +1,36 @@
 import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
 import Dashboard from './components/Dashboard/Dashboard.jsx';
 import Login from './components/Login/Login.jsx';
+import SignUp from './components/SignUp/SignUp.jsx';
 import Preferences from './components/Preferences/Preferences.jsx';
-import axios from 'axios';
-
-const apiCall = () => {
-  axios.get('http://localhost:8000').then((data) => {
-    //this console.log will be in our frontend console
-    console.log(data)
-  })
-}
 
 function App() {
-  const [token, setToken] = useState();
+  const [userId, setUserId] = useState(localStorage.getItem('userId'));
 
-  
   return (
     <BrowserRouter>
-      {!token ? (
-        <Login setToken={setToken} />
-      ) :(
+      <Routes>
 
-      <div className="wrapper">
-        <h1>Application</h1>
-        <button onClick={apiCall}>Make API Call</button>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/preferences" element={<Preferences />} />
-          </Routes>
-      </div>
-      )}
+        <Route path="/login" element={<Login setUserId={setUserId} />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        <Route
+          path="/dashboard"
+          element={userId ? <Dashboard /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/preferences"
+          element={userId ? <Preferences /> : <Navigate to="/login" replace />}
+        />
+
+        <Route path="/" element={<Navigate to={userId ? "/dashboard" : "/login"} replace />} />
+      </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
